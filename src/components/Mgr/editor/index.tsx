fd0161b26f9228f720e69d9b9fa04d44ed4626f4
Goc-yan <632114989@qq.com>
2019-03-15 12:03:38 +0800

    add richtext

--- src/components/Mgr/editor/index.tsx
+++ src/components/Mgr/editor/index.tsx
@@ -1,11 +1,16 @@
 import * as React from 'react'
 
 
-import { Form, Input, Select, Button, Tag } from 'antd'
+import { Layout, Form, Input, Select, Button, Tag } from 'antd'
 
 // import * as marked from 'marked';
 // import * as hljs from 'highlight.js';
 // import Editor from 'for-editor'
+
+import ReactQuill from 'react-quill'
+import 'react-quill/dist/quill.snow.css'
+
+import './style.css'
 
 const { Option } = Select
 const CheckableTag = Tag.CheckableTag
@@ -16,7 +21,9 @@ interface State {
   confirmDirty: boolean
   autoCompleteResult: string[]
   selectedTags: string[]
-  articleDetail: any
+  text?: any
+  modules: any
+  formats: string[]
 }
 
 class RegistrationForm extends React.Component<any, State> {
@@ -28,8 +35,26 @@ class RegistrationForm extends React.Component<any, State> {
       confirmDirty: false,
       autoCompleteResult: [],
       selectedTags: [],
-      articleDetail: '',
+      text: '',
+      modules: {
+        toolbar: [
+          [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+          ['bold', 'italic', 'underline', 'strike', 'blockquote', 'code'],
+          [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'indent': '-1' }, { 'indent': '+1' }],
+          ['link', 'image'],
+          ['clean']
+        ],
+      },
+      formats: [
+        'header',
+        'bold', 'italic', 'underline', 'strike', 'blockquote', 'code',
+        'list', 'bullet', 'indent',
+        'link', 'image'
+      ],
     }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleRichChange = this.handleRichChange.bind(this)
   }
 
   handleSubmit = (e: any) => {
@@ -56,9 +81,11 @@ class RegistrationForm extends React.Component<any, State> {
     this.setState({ selectedTags: nextSelectedTags })
   }
 
-  componentWillMount() {
-
+  handleRichChange(value: any): void {
+    this.setState({ text: value })
   }
+
+  componentWillMount() { }
 
   render() {
     const { getFieldDecorator } = this.props.form
@@ -72,48 +99,44 @@ class RegistrationForm extends React.Component<any, State> {
     }
 
     return (
-      <>
-        <Form {...formItemLayout} onSubmit={this.handleSubmit}>
-          <Form.Item label="标题" >
-            {getFieldDecorator('title', {
-              rules: [{
-                required: true, message: 'Please input article title',
-              }],
-            })(<Input />)}
-          </Form.Item>
-          <Form.Item label="分类" >
-            <Select defaultValue="1">
-              <Option value="1">Option 1</Option>
-              <Option value="2">Option 2</Option>
-              <Option value="3">Option 3</Option>
-            </Select>
-          </Form.Item>
-
-          <Form.Item label="标签" >
-            {tagsFromServer.map(tag => (
-              <CheckableTag
-                key={tag}
-                checked={this.state.selectedTags.indexOf(tag) > -1}
-                onChange={checked => this.handleChange(tag, checked)}
-              >
-                {tag}
-              </CheckableTag>
-            ))}
-          </Form.Item>
-          <Form.Item {...tailFormItemLayout}>
-            <Button type="primary" htmlType="submit">保存</Button>
-          </Form.Item>
-        </Form>
-        {/* <div className="content">
-          <div
-            id="content"
-            className="article-detail"
-            dangerouslySetInnerHTML={{
-              __html: this.state.articleDetail.content ? marked(this.state.articleDetail.content) : null
-            }}
-          />
-        </div> */}
-      </>
+      <Form {...formItemLayout} onSubmit={this.handleSubmit}>
+        <Form.Item label="标题" >
+          {getFieldDecorator('title', {
+            rules: [{
+              required: true, message: 'Please input article title',
+            }],
+          })(<Input />)}
+        </Form.Item>
+        <Form.Item label="分类" >
+          <Select defaultValue="1">
+            <Option value="1">Option 1</Option>
+            <Option value="2">Option 2</Option>
+            <Option value="3">Option 3</Option>
+          </Select>
+        </Form.Item>
+        <Form.Item label="标签" >
+          {tagsFromServer.map(tag => (
+            <CheckableTag
+              key={tag}
+              checked={this.state.selectedTags.indexOf(tag) > -1}
+              onChange={checked => this.handleChange(tag, checked)}
+            >
+              {tag}
+            </CheckableTag>
+          ))}
+        </Form.Item>
+        <Form.Item label="正文">
+          <ReactQuill
+            theme="snow"
+            modules={this.state.modules}
+            formats={this.state.formats}
+            value={this.state.text}
+            onChange={this.handleRichChange} />
+        </Form.Item>
+        <Form.Item {...tailFormItemLayout}>
+          <Button type="primary" htmlType="submit">保存</Button>
+        </Form.Item>
+      </Form>
     )
   }
 }
