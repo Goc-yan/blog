90c7ab53ca6ad6da50726ff0544a2b2e49e8c401
Goc-yan <632114989@qq.com>
2019-03-26 23:22:17 +0800

    optimization CORS

--- app.js
+++ app.js
@@ -1,64 +1,62 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require('http-errors')
+var express = require('express')
+var path = require('path')
+var cookieParser = require('cookie-parser')
+var logger = require('morgan')
 
-var indexRouter = require('./routes/index');
-var mgrRouter = require('./routes/mgr');
-// var add = require('./db/user');
-
+var indexRouter = require('./routes/index')
+var mgrRouter = require('./routes/mgr')
 var api = require('./api')
 
-var app = express();
+var app = express()
 
 // view engine setup
-app.set('views', path.join(__dirname, 'dist', 'pages'));
-app.engine('html', require('ejs').__express);
-app.set('view engine', 'html');
+app.set('views', path.join(__dirname, 'dist', 'pages'))
+app.engine('html', require('ejs').__express)
+app.set('view engine', 'html')
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'dist')))
 
 
 app.all('*', function (req, res, next) {
   // res.header("Access-Control-Allow-Credentials", true)
   res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type")
   res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS")
-  // res.header("X-Powered-By", ' 3.2.1')
-  // res.header("Content-Type", "application/json;charset=utf-8")
-  next()
+  res.header("X-Powered-By", ' 3.2.1')
+  res.header("Content-Type", "application/jsoncharset=utf-8")
+
+  if (req.method === 'OPTIONS') res.sendStatus(200)
+  else next()
 })
 
 
 
 app.use('/api', api)
-app.use('/', indexRouter);
-app.use('/mgr', mgrRouter);
-// app.use('/add', add); //增
-
+app.use('/', indexRouter)
+app.use('/mgr', mgrRouter)
 
 
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
-});
+  next(createError(404))
+})
 
 
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  res.status(err.status || 500)
+  res.render('error')
+})
 
-module.exports = app;
+module.exports = app
